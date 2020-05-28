b2c3c90e5c55415101ba46f3eb437b96ab40b8cb
AaronYin0514 <562540603@qq.com>
2020-05-28 23:05:10 +0800

    版本

--- release.sh
+++ release.sh
@@ -1,39 +1,9 @@
 #!/bin/bash
 
-echo "添加标签$1"
-git tag $1
-echo "添加标签$1成功!"
+#pod repo remove AaronOC
 
-echo "推送标签$1"
-git push origin $1
-echo "推送标签$1成功!"
+#pod repo add AaronSpecs https://github.com/AaronYin0514/AaronSpecs.git
 
-echo "删除本地AaronOC库"
+pod repo push AaronSpecs AaronOC.podspec  --sources=https://github.com/AaronYin0514/AaronSpecs.git --allow-warnings --verbose
 
-pod repo remove AaronOC
 
-echo "本地AaronOC库——已删除"
-
-echo "添加远程仓库到本地"
-
-pod repo add AaronOC https://github.com/AaronYin0514/AaronOC.git
-
-echo "添加远程仓库完成"
-
-echo "发布新版本"
-
-pod repo push AaronOC AaronOC.podspec --allow-warnings
-
-echo "发布完成"
-
-if [ -d "/Users/yinzhongzheng/.cocoapods/repos/AaronOC/AaronOC/Classes/" ]; then
-        echo "包含Class文件夹，正在删除"
-        rm -rf /Users/yinzhongzheng/.cocoapods/repos/AaronOC/AaronOC/Classes
-        echo "删除Class文件夹完成"
-fi
-
-if [ -d "/Users/yinzhongzheng/.cocoapods/repos/AaronOC/AaronOC/Assets/" ]; then
-        echo "包含Assets文件夹"
-        rm -rf /Users/yinzhongzheng/.cocoapods/repos/AaronOC/AaronOC/Assets
-        echo "删除Assets文件夹完成"
-fi
